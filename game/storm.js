9450b211325d8cd5c9e633dcf473b7f79690bd46
joshuakernich <joshua.kernich@gmail.com>
2021-02-14 18:26:03 +1100

    Finalised imports, music, and lighting cues

--- game/storm.js
+++ game/storm.js
@@ -5,6 +5,7 @@ $(function(){
 	const H = 257;
 	const FRAMES = 91;
 
+	var iTick;
 	var $s = $('.storm');
 	var offset = {'background-position-x':0,'background-position-y':0};
 	var iTick = 0;
@@ -12,19 +13,32 @@ $(function(){
 
 	var lightning = [5,6,8,9,10,11,52,53,54,55,56,57,58];
 
-	setInterval(tick,1000/12);
-
 	function tick(){
 		iTick++;
 		iTick = iTick%FRAMES;
-		offset['background-position-x'] = -(iTick%columns)*W + 'px';
-		offset['background-position-y'] = -Math.floor(iTick/columns)*H + 'px';
+
+		offset['background-position-x'] = -(iTick%columns)*100 + '%';
+		offset['background-position-y'] = -Math.floor(iTick/columns)*100 + '%';
 		$s.css(offset);
 
-		if(lightning.indexOf(iTick+1)>-1) clapThatAss();
+		if(iTick==3) MIDI.send(MIDI.LIGHTNING_STRIKE_S);
+		if(iTick==6) MIDI.send(MIDI.LIGHTNING_STRIKE_M);
+		if(iTick==51) MIDI.send(MIDI.LIGHTNING_STRIKE_L);
 	}
 
-	function clapThatAss(){
-		
+	MIDI.toStorm = function(){
+		iTick = 0;
+		tick();
+		iTick = setInterval(tick,1000/12);
+		$s.show();
+		MIDI.senf(MIDI.NIGHT_MODE);
 	}
+
+	MIDI.toCalm = function(){
+		clearInterval(iTick);
+		$s.hide();
+		MIDI.send(MIDI.DAY_MODE);
+		console.log('here');
+	}
+
 })
